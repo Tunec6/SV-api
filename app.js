aa6916ae09f5861cd3fcf8a39c2cbc3560791829
Tunec <vadimstepanov04@gmail.com>
2023-05-31 11:58:38 +0500

    post

--- app.js
+++ app.js
@@ -1,81 +1,57 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const sqlite = require("sqlite3")
+const md5 = require("md5")
+const cors = require("cors")
 const db = new sqlite.Database('./db/list.db')
 
-var app = express();
+const port = 4000
+
+const app = express();
 app.use(bodyParser.urlencoded())
 app.use(bodyParser.json())
 
-app.listen(8000, () => {
-    console.log("Сервер запущен на порту 8000.")
+app.listen(port, () => {
+    console.log("Сервер запущен на порту " + `${port}`)
 })
 
-app.get('/', (req, res) => {
-    res.json('yes')
-})
-
-app.get('/todo',(req, res) =>{
-    db.all(`SELECT * FROM todo`,(err, rows) =>{
+app.get('/list/questions', (req, res) => {
+    db.all(`SELECT * FROM questions`, (err, rows) => {
         res.json(rows)
-        
+
     })
 })
 
-app.get('/todo/:id',(req, res) =>{
-    const {id} = req.params
-    db.all(`SELECT * FROM todo WHERE id=${id}`,(err, rows) =>{
-        res.json(rows)
-        
-    })
-})
 
-app.put('/todo/:id',(req, res) =>{
-    const {id} = req.params
-    const {Date} = req.body
-    const {task} = req.body
-    const {description} = req.body
-    const {status} = req.body
-    const request = `UPDATE todo SET date = "${Date}", task = "${task}", description = "${description}", status = "${status}" WHERE id=${id}`
+app.post('/list/questions', (req, res) => {
+    const questions = req.body
+    const request = `INSERT INTO questions VALUES(
+    null,
+    '${questions.question}'
+    )`
+    console.log(request)
 
-
-    db.run(request, (err) =>{
-        if (err){
-            res.json(err)
-        }
-        res.json('задача обновлена')
-    })
-})
-
-app.delete('/todo/:id',(req, res) =>{
-    const {id} = req.params
-    const request = `DELETE FROM todo WHERE 
-        id=${id}`
-        
-        
-        db.run(request, (err) => {
-            if(err) {
-                res.json(err)
-            }
-            res.json('задача удалена')
-        })
-})
-
-app.post('/todo',(req, res) =>{
-    const data = req.body
-    const request = `INSERT INTO todo VALUES (
-        null,
-        '${data.Date}',
-        '${data.task}',
-        '${data.description}',
-        '${data.status}')`
-
-       
-
-    db.run(request,(err) =>{
+    db.run(request, (err) => {
         if (err) {
             res.json(err)
         }
         res.json('добавлено')
+    })
+})
+
+app.post('/list/answer', (req, res) => {
+    const answer = req.body
+    const request = `INSERT INTO answer VALUES(
+        null,
+        '${answer.question_id}'
+        '${answer.answer}',
+        '${answer.is_correct}'
+    )`
+
+    db.run(request, (err) => {
+        if (err) {
+            res.json(err)
+        }
+        res.json('ответы добавлены')
     })
 })
